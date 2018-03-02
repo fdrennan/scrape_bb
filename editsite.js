ce49265ae89a07fef7ad9ad721df51e9dbad5c48
Freddy Drennan <drennanfreddy@gmail.com>
2018-03-01 22:31:05 -0700

    update

--- editsite.js
+++ editsite.js
@@ -3,7 +3,7 @@ var webPage = require('webpage')
 var page = webPage.create();
 var fs = require('fs');
 var path ='website.html'
-page.open('https://www.airbnb.com/s/homes?allow_override%5B%5D=&refinement_paths%5B%5D=%2Fhomes&ne_lat=30.97855687613024&ne_lng=-97.19728718511101&sw_lat=29.90656374808267&sw_lng=-98.21626911870476&search_by_map=true&zoom=9&s_tag=l5HL7UMh&section_offset=2', function (status) {
+page.open('https://www.airbnb.com/s/homes?allow_override%5B%5D=&refinement_paths%5B%5D=%2Fhomes&ne_lat=30.97855687613024&ne_lng=-97.19728718511101&sw_lat=29.90656374808267&sw_lng=-98.21626911870476&search_by_map=true&zoom=9&s_tag=l5HL7UMh&section_offset=7', function (status) {
   var content = page.content;
  fs.write(path,content,'w')
   phantom.exit();
--- runjs.R
+++ runjs.R
@@ -1,5 +1,17 @@
 # # Explanation here: https://www.r-bloggers.com/web-scraping-javascript-rendered-sites/
 
+mongo_connect = function(collectionName, dbName) {
+  # Cred
+  hostName = "mongodb://fdrennan:thirdday1@drenr.com:27017"
+  
+  m <- mongolite::mongo(collection = collectionName , 
+                        db = dbName, 
+                        url = hostName)
+  
+  m
+}
+
+m = mongo_connect('bnb', 'datasets')
 
 library(tidyverse)
 library(rvest)
@@ -13,7 +25,7 @@ long = 50
 zoom = 9
 
 site = "https://www.airbnb.com/s/homes?allow_override%5B%5D=&refinement_paths%5B%5D=%2Fhomes&ne_lat=30.97855687613024&ne_lng=-97.19728718511101&sw_lat=29.90656374808267&sw_lng=-98.21626911870476&search_by_map=true&zoom=9&s_tag=l5HL7UMh&section_offset="
-site = paste0(site, 1:16)
+site = paste0(site, 7:16)
 output = "website.html"
 
 
@@ -53,7 +65,8 @@ for(i in seq_along(site)) {
       str_sub(start = 16) %>% 
       gsub("&.*", "", .)
     
-    page_tibble[i,3] = link
+    page_tibble[j,3] = link
+    print(page_tibble)
   }
   
   
@@ -69,6 +82,10 @@ for(i in seq_along(site)) {
   } else {
     page_tibble_df = bind_rows(page_tibble_df, page_tibble)
   }
+  page_tibble$time = Sys.time()
+  page_tibble$i = i
+  page_tibble$j = j
+  m$insert(page_tibble)
   print(page_tibble_df)
 }
 
